83d86c530a09a70a597eaedee84aa682cdcd599b
lvxinwei <lyuxinwei@gmail.com>
2015-09-28 16:40:55 +0800

    s

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 _author__ = 'lvxinwei'
+from QQCore import *
+qq=QQCore(False)
+qq.login_by_qrcode()
+while True:
+    qq.check_msg()
+    time.sleep(2)
