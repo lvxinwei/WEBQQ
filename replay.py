e390f6b5397a8157c67090eaa7e7539805b471d4
lvxinwei <lyuxinwei@gmail.com>
2015-09-28 16:48:11 +0800

    回复测试

--- replay.py
+++ replay.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 _author__ = 'lvxinwei'
+from QQCore import *
+qq=QQCore(True)
+qq.getFriendInfo()
+print qq.replay(qq.friendList[857280627]['uin'],'hell')
