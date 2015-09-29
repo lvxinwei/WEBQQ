0aa03e04fc673039e0615d2c671d00838cae6e0a
lvxinwei <lyuxinwei@gmail.com>
2015-09-29 15:23:33 +0800

    增加msg_id 消息排序

--- MsgHandle.py
+++ MsgHandle.py
@@ -4,11 +4,15 @@ _author__ = 'lvxinwei'
 import json
 from QQCore import *
 import redis,time
+def sortMsg(a,b):
+    a=":".join(a.split(":")[0:-1])
+    b=":".join(b.split(":")[0:-1])
+    return cmp(a,b)
 class MsgHandle:
     def __init__(self):
         self.qq=QQCore(True)
         self.qq.getFriendInfo()
-        self.redis=redis.StrictRedis(host='lvxinwei.com', port=6379, db=0)
+        self.redis=redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
         pass
     def __getValue(self,key):
         return self.redis.get(key)
@@ -24,34 +28,51 @@ class MsgHandle:
         return self.redis.rpop(key)
     def __lrange(self,key,start=0,stop=-1):
         return self.redis.lrange(key,start,stop)
+    def __getLikeKeys(self,key):
+        return self.redis.keys(key)
+    def getMsgByAccount(self,account):
+        likeKey='message:'+str(account)+"*"
+        keys=self.__getLikeKeys(likeKey)
+        #对keys排序
+        keys=sorted(keys,sortMsg)
+        ret=[]
+        for key in keys:
+            ret.append([key,self.__getValue(key)])
+        return ret
+    def getLastMsgId(self,account):
+        lastMsgKey=self.getMsgByAccount(account)[-1][0]
+        return int(lastMsgKey.split(":")[-2])
     def start(self):
-        msg_in=self.__lpop('message_box_in1')
+        msg_in=self.__lpop('message_box_in')
         msg_out=self.__lpop('message_box_out')
         self.handleMsgIn(msg_in)
-        print msg_out
         self.handleMsgOut(msg_out)
     def handleMsgOut(self,msg):
         if not msg:
             return
         msg=json.loads(msg)
         account=msg['account']
+        msg_id=self.getLastMsgId(account)+1
+        print msg_id
         time=msg['time']
         content=msg['content']
-        uin=self.qq.friendList[account]
-        if self.qq.replay(uin,account):
+        uin=self.qq.friendList[account]['uin']
+        if self.qq.replay(uin,content,msg_id):
+            key="message:"+str(account)+":"+time+":"+str(msg_id)+":out"
+            self.__setValue(key,content)
             return True
         else :
             print 'replay error'
     def handleMsgIn(self,msg):
         if not msg:
             return
-        json.loads(msg)
+        msg=json.loads(msg)
         poll_type=msg['poll_type']
         if poll_type=='message':
-            ret=self.__handleMessage(msg)
+            ret=self.__handleNormalMessage(msg)
             self.__setValue(ret[0],ret[1])
         if poll_type=='kick_message':
-            ret=self.__kickMessage(msg)
+            ret=self.__handleKickMessage(msg)
             self.__setValue(ret[0],ret[1])
     def __combineMsg(self,content):
         msgtxt = ""
@@ -65,7 +86,7 @@ class MsgHandle:
                 elif str(part[0]) == "cface":
                     msgtxt += "[表情]"
         return msgtxt
-    def __handleMessage(self,msg):
+    def __handleNormalMessage(self,msg):
         poll_type=msg['poll_type']
         time=str(msg['value']['time'])
         account=self.qq.getAccountByUin(msg['value']['from_uin'])
@@ -75,11 +96,15 @@ class MsgHandle:
         content=self.__combineMsg(msg['value']['content'])
         key=poll_type+":"+str(account)+":"+str(time)+":"+str(msg_id)+":in"
         return [key,content]
-    def __kickMessage(self,msg):
+    def __handleKickMessage(self,msg):
         key=msg['poll_type']
         key=key+":"+str(int(time.time()))
         return [key,msg['value']['reason']]
 
 if __name__=='__main__':
     msgh=MsgHandle()
-    msgh.start()
+    # while True:
+    #     msgh.start()
+    #     time.sleep(1)
+    #print msgh.getLastMsgId(857280627)
+    print msgh.getMsgByAccount(857280627)
